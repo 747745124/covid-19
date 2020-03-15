f10e1e6d46f28bcfa476b85a59bcb8e25d8e83f8
NAOYUKI <zhaonzh@gmail.com>
2020-03-16 00:36:00 +0800

    copy

---
+++ README.md
@@ -0,0 +1,18 @@
+## COVID-19 Coronavirus Map / 新型冠状病毒肺炎疫情图
+
+<img src="https://raw.githubusercontent.com/stevenliuyi/covid19/master/public/cover.png" width="400" />
+
+COVID-19 (2019-nCOV / SARS-CoV-2) Coronavirus Map (https://covid19.health) is an interactive, animated map for the COVID-19 coronavirus outbreak.
+
+If you'd like to support the work and buy me a ☕, I greatly appreciate it!
+
+<a href="https://www.buymeacoffee.com/stevenliuyi" target="_blank"><img src="https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png" alt="Buy Me A Coffee" style="height: 41px !important;width: 174px !important;box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;-webkit-box-shadow: 0px 3px 2px 0px rgba(190, 190, 190, 0.5) !important;" ></a>
+### Data Sources
+- Worldwide/United States/Australia: [2019 Novel Coronavirus COVID-19 (2019-nCoV) Data Repository by Johns Hopkins CSSE](https://github.com/CSSEGISandData/COVID-19)
+- China: [COVID-19/2019-nCoV Time Series Infection Data Warehouse](https://github.com/BlankerL/DXY-COVID-19-Data) (data crawled from [Ding Xiang Yuan](https://ncov.dxy.cn/ncovh5/view/pneumonia))
+- United States (county level): [1Point3Acres COVID-19 in US and Canada](https://coronavirus.1point3acres.com/en)
+- Italy: [Dati COVID-19 Italia](https://github.com/pcm-dpc/COVID-19)
+- South Korea: [parksw3/COVID19-Korea](https://github.com/parksw3/COVID19-Korea)
+- France: [cedricguadalupe/FRANCE-COVID-19](https://github.com/cedricguadalupe/FRANCE-COVID-19)
+- Germany/Austria: [covid19-eu-zh/covid19-eu-data](https://github.com/covid19-eu-zh/covid19-eu-data)
+- Japan: [新型コロナウイルス感染速報](https://github.com/swsoyee/2019-ncov-japan)
---
+++ postinstall.sh
@@ -0,0 +1,85 @@
+#!/bin/bash
+
+mkdir -p ./data/maps
+mkdir -p ./public/maps
+
+# download maps
+wget -nc https://biogeo.ucdavis.edu/data/gadm3.6/shp/gadm36_CHN_shp.zip -O ./data/maps/gadm36_CHN_shp.zip
+unzip -o -d ./data/maps/ ./data/maps/gadm36_CHN_shp.zip
+wget -nc https://biogeo.ucdavis.edu/data/gadm3.6/shp/gadm36_HKG_shp.zip -O ./data/maps/gadm36_HKG_shp.zip
+unzip -o -d ./data/maps/ ./data/maps/gadm36_HKG_shp.zip
+wget -nc https://biogeo.ucdavis.edu/data/gadm3.6/shp/gadm36_MAC_shp.zip -O ./data/maps/gadm36_MAC_shp.zip
+unzip -o -d ./data/maps/ ./data/maps/gadm36_MAC_shp.zip
+wget -nc https://biogeo.ucdavis.edu/data/gadm3.6/shp/gadm36_TWN_shp.zip -O ./data/maps/gadm36_TWN_shp.zip
+unzip -o -d ./data/maps/ ./data/maps/gadm36_TWN_shp.zip
+wget -nc https://biogeo.ucdavis.edu/data/gadm3.6/shp/gadm36_KOR_shp.zip -O ./data/maps/gadm36_KOR_shp.zip
+unzip -o -d ./data/maps/ ./data/maps/gadm36_KOR_shp.zip
+wget -nc https://biogeo.ucdavis.edu/data/gadm3.6/shp/gadm36_ITA_shp.zip -O ./data/maps/gadm36_ITA_shp.zip
+unzip -o -d ./data/maps/ ./data/maps/gadm36_ITA_shp.zip
+wget -nc https://biogeo.ucdavis.edu/data/gadm3.6/shp/gadm36_FRA_shp.zip -O ./data/maps/gadm36_FRA_shp.zip
+unzip -o -d ./data/maps/ ./data/maps/gadm36_FRA_shp.zip
+wget -nc https://biogeo.ucdavis.edu/data/gadm3.6/shp/gadm36_DEU_shp.zip -O ./data/maps/gadm36_DEU_shp.zip
+unzip -o -d ./data/maps/ ./data/maps/gadm36_DEU_shp.zip
+wget -nc https://biogeo.ucdavis.edu/data/gadm3.6/shp/gadm36_JPN_shp.zip -O ./data/maps/gadm36_JPN_shp.zip
+unzip -o -d ./data/maps/ ./data/maps/gadm36_JPN_shp.zip
+wget -nc https://biogeo.ucdavis.edu/data/gadm3.6/shp/gadm36_AUT_shp.zip -O ./data/maps/gadm36_AUT_shp.zip
+unzip -o -d ./data/maps/ ./data/maps/gadm36_AUT_shp.zip
+wget -nc https://biogeo.ucdavis.edu/data/gadm3.6/shp/gadm36_AUS_shp.zip -O ./data/maps/gadm36_AUS_shp.zip
+unzip -o -d ./data/maps/ ./data/maps/gadm36_AUS_shp.zip
+wget -nc https://biogeo.ucdavis.edu/data/gadm3.6/shp/gadm36_USA_shp.zip -O ./data/maps/gadm36_USA_shp.zip
+unzip -o -d ./data/maps/ ./data/maps/gadm36_USA_shp.zip
+
+wget -nc https://raw.githubusercontent.com/zcreativelabs/react-simple-maps/master/topojson-maps/world-50m-simplified.json -O ./data/maps/world-50m.json
+
+wget -nc https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json -O ./data/maps/states-10m.json
+
+# simplify maps
+./node_modules/mapshaper/bin/mapshaper ./data/maps/gadm36_CHN_1.shp -simplify 2% -clean -o format=topojson ./data/maps/gadm36_CHN_1.json
+./node_modules/mapshaper/bin/mapshaper ./data/maps/gadm36_CHN_2.shp -simplify 2% -clean -o format=topojson ./data/maps/gadm36_CHN_2.json
+./node_modules/mapshaper/bin/mapshaper ./data/maps/gadm36_HKG_0.shp -simplify 0.2% -clean -o format=topojson ./data/maps/gadm36_HKG_0.json
+./node_modules/mapshaper/bin/mapshaper ./data/maps/gadm36_MAC_0.shp -simplify 3% -clean -o format=topojson ./data/maps/gadm36_MAC_0.json
+./node_modules/mapshaper/bin/mapshaper ./data/maps/gadm36_TWN_0.shp -simplify 0.5% -clean -o format=topojson ./data/maps/gadm36_TWN_0.json
+./node_modules/mapshaper/bin/mapshaper ./data/maps/gadm36_KOR_1.shp -simplify 0.5% -clean -o format=topojson ./public/maps/gadm36_KOR_1.json
+./node_modules/mapshaper/bin/mapshaper ./data/maps/gadm36_ITA_1.shp -simplify 1% -clean -o format=topojson ./public/maps/gadm36_ITA_1.json
+./node_modules/mapshaper/bin/mapshaper ./data/maps/gadm36_FRA_1.shp -simplify 1% -clean -o format=topojson ./public/maps/gadm36_FRA_1.json
+./node_modules/mapshaper/bin/mapshaper ./data/maps/gadm36_DEU_1.shp -simplify 1% -clean -o format=topojson ./public/maps/gadm36_DEU_1.json
+./node_modules/mapshaper/bin/mapshaper ./data/maps/gadm36_JPN_1.shp -simplify 0.5% -clean -o format=topojson ./public/maps/gadm36_JPN_1.json
+./node_modules/mapshaper/bin/mapshaper ./data/maps/gadm36_AUT_1.shp -simplify 5% -clean -o format=topojson ./public/maps/gadm36_AUT_1.json
+./node_modules/mapshaper/bin/mapshaper ./data/maps/gadm36_AUS_1.shp -simplify 0.2% -clean -o format=topojson ./public/maps/gadm36_AUS_1.json
+./node_modules/mapshaper/bin/mapshaper ./data/maps/gadm36_USA_2.shp -filter 'TYPE_2 != "Water body"' -simplify 0.5% -clean -o format=topojson ./public/maps/gadm36_USA_2.json
+
+./node_modules/mapshaper/bin/mapshaper ./data/maps/world-50m.json -filter 'NAME != "Antarctica"' -simplify 50% -clean -o format=topojson ./public/maps/world-50m.json
+./node_modules/mapshaper/bin/mapshaper ./data/maps/states-10m.json -simplify 50% -clean -o format=topojson ./public/maps/states-10m.json
+
+# combine maps
+./node_modules/mapshaper/bin/mapshaper -i ./data/maps/gadm36_CHN_1.json ./data/maps/gadm36_HKG_0.json ./data/maps/gadm36_MAC_0.json ./data/maps/gadm36_TWN_0.json combine-files -merge-layers force -o format=topojson ./public/maps/gadm36_CHN_1.json
+./node_modules/mapshaper/bin/mapshaper -i ./data/maps/gadm36_CHN_2.json ./data/maps/gadm36_HKG_0.json ./data/maps/gadm36_MAC_0.json ./data/maps/gadm36_TWN_0.json combine-files -merge-layers force -o format=topojson ./public/maps/gadm36_CHN_2.json
+
+# reverse DXY data file so that the lastest record is in the end instead of beginning
+tac data/dxy-data/csv/DXYArea.csv > data/DXYArea_reversed.csv
+
+# download data files for South Korea cases
+mkdir -p data/korea-data
+wget --no-check-certificate 'https://docs.google.com/spreadsheets/d/1nKRkOwnGV7RgsMnsYE6l96u4xxl3ZaNiTluPKEPaWm8/export?gid=0&format=csv' -O data/korea-data/line_list.csv
+wget --no-check-certificate 'https://docs.google.com/spreadsheets/d/1nKRkOwnGV7RgsMnsYE6l96u4xxl3ZaNiTluPKEPaWm8/export?gid=898304475&format=csv' -O data/korea-data/geo_distribution.csv
+wget --no-check-certificate 'https://docs.google.com/spreadsheets/d/1nKRkOwnGV7RgsMnsYE6l96u4xxl3ZaNiTluPKEPaWm8/export?gid=306770783&format=csv' -O data/korea-data/cumulative_numbers.csv
+
+# data folder
+mkdir -p public/data
+
+# generate data in JSON format and include data in TOPOJSON maps
+python3 data/1p3a-data/crawler.py
+node src/scripts/data_processing_world.js
+node src/scripts/data_processing_china.js
+node src/scripts/data_processing_korea.js
+node src/scripts/data_processing_italy.js
+node src/scripts/data_processing_us.js
+node src/scripts/data_processing_us_1p3a.js
+node src/scripts/data_processing_france.js
+node src/scripts/data_processing_germany.js
+node src/scripts/data_processing_japan.js
+node src/scripts/data_processing_austria.js
+node src/scripts/data_processing_australia.js
+
+# merge data
+node src/scripts/data_merge.js
